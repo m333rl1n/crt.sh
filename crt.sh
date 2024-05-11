a8f541797886d3f44cc703d048749ac5f1750155
Merlin <141517503+m333rl1n@users.noreply.github.com>
2024-05-12 00:16:01 +0330

    Update crt.sh

--- crt.sh
+++ crt.sh
@@ -1,26 +1,18 @@
 #! /bin/bash
 
-# defautl query
-q='.common_name'
+query='.common_name'
 
-# iterate args
-for i in "$@"; do
-  case $i in
-    -h|--help)
-      # show help message
-      echo "USAGE: $0 [target] [-b|--both]"
-      echo -e "\nSearch in crt.sh for domains and subdomains with organization name."
-      echo -e "\nSwitches:"
-      echo -e "\t-b,--both\tshow both 'comman_name' and 'name_value' of certificate"
-      exit 0
-      ;;
+if [ $# -lt 1 ]; then
+    echo "USAGE: $0 [target] [-a|-all]"
+    echo -e "\nSearch in crt.sh for domains and subdomains with organization name."
+    echo -e "\nSwitches:"
+    echo -e "\t-a,-all\tshow 'comman_name' and 'name_value' of certificate"
+    exit 0
+fi
 
-    -b|--both)
-        # update query and add name value to result
-        q=$q'+"\n"+.name_value'
-        ;;
-    esac
-done
+if [ "$2" = "-a" ] || [ "" = "-all" ]; then
+    query+='+"\n"+.name_value' # update query
+fi
 
 # Send request and clean data
-curl -s "https://crt.sh/?q=$1&output=json" | jq -r ".[] | $q"
+curl -s "https://crt.sh/?q=$1&output=json" | jq -r ".[] | $query"
